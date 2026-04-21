6eaa518c2d2c7640b3c3d55ba265b12a7bc44567
Chiranjib Sai Chandan Nath <sanunath2440@gmail.com>
2026-04-21 16:03:56 +0530

    Change application access URL to localhost

--- run.py
+++ run.py
@@ -5,7 +5,8 @@ app = create_app()
 if __name__ == '__main__':
     print("Starting Malware Analysis Sandbox...")
     print("Frontend and Backend are running together.")
-    print("Access the application at: http://192.168.171.128:5000")
+    print("Access the application at: http://127.0.0.1:5000")
 
-    # host='0.0.0.0' allows Windows browser to connect to Kali
-    app.run(host='0.0.0.0', debug=True, port=5000)
+
+    app.run(host='127.0.0.1', debug=True, port=5000)
+
